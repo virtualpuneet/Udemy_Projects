2bd5412f8c8591874ca0836ac6502632f394df8c
virtualpuneet <45757139+virtualpuneet@users.noreply.github.com>
2025-08-24 13:06:52 +0800

    Day2.3

---
+++ Day 2/Variable.py
@@ -0,0 +1,11 @@
+name = input ("Tell us your name: ")
+print ("My name is " + name)
+
+age = input ("How old are you? ")
+print ("You are " + age + " years old.")
+height = input ("How tall are you? ")
+print ("You are " + height + " tall.")
+weight = input ("How much do you weigh? ")
+print ("You weigh " + weight + " kgs.")
+
+
