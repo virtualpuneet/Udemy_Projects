81f0274bb59237a69f639ca69103308bbcadbece
virtualpuneet <45757139+virtualpuneet@users.noreply.github.com>
2025-08-24 12:10:42 +0800

    day1.2

--- Frst_project.py
+++ Frst_project.py
@@ -1,11 +1,11 @@
-print("hello world, im saying 'hello'")
-print ("hello" + " puneet")
-print ("i learnt something new,\nwhich i need to practice")
-print("\tnow lets try with some indentation")
-print ("i wanted to print a \ backslash")
-i=0
-while (i <=5):
-    print("\t"*i, "this is ", i)
-    i=i+1
+#print("hello world, im saying 'hello'")
+#print ("hello" + " puneet")
+#print ("i learnt something new,\nwhich i need to practice")
+#print("\tnow lets try with some indentation")
+#print ("i wanted to print a \ backslash")
+#i=0
+#while (i <=5):
+#    print("\t"*i, "this is ", i)
+#    i=i+1
 
-
+input("What are you learning today?")
